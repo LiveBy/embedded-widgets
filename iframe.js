bccf41800f16276f975384ecf052c6d7dfe5de59
Malachi <malachimart@gmail.com>
2016-01-04 11:15:09 -0600

    added tracking codes for hotjar and google analytics

--- iframe.js
+++ iframe.js
@@ -6,8 +6,27 @@
  * After DOM load, this script will generate an iframe at the embedded hook (<div class="liveBy-container"></div>)
  */
 
-// Hotjar Snippet HERE for heatmap.
+//hotjar and ga tracking codes
 
+(function(h,o,t,j,a,r){
+		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+		h._hjSettings={hjid:106041,hjsv:5};
+		a=o.getElementsByTagName('head')[0];
+		r=o.createElement('script');r.async=1;
+		r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+		a.appendChild(r);
+	})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	ga('create', 'UA-71267055-3', 'auto');
+	ga('send', 'pageview');
+
+
+
+// iframe generation
 
 document.addEventListener('DOMContentLoaded', function (event) {
   var attributes = {
